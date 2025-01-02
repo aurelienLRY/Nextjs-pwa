a41e14333e1d47794ef962d65fffe833847c4845
aurelienLRY <83220559+aurelienLRY@users.noreply.github.com>
2025-01-02 15:56:24 +0100

    Integrate PWA support and update configuration
    - Added `next-pwa` for Progressive Web App support in `next.config.ts`, enabling service worker registration and caching.
    - Updated `package.json` and `package-lock.json` to include `next-pwa` and its TypeScript types.
    - Created a new manifest file (`manifeste.ts`) to define app metadata and icons for PWA.
    - Added Android Chrome icons for PWA in the public/icons directory.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+import withPWA from "next-pwa";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
-  }
+    domains: ["images.unsplash.com"],
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
