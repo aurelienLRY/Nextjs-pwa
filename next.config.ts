05904f1c58dbf94ed93e5e8471a33433134be258
aurelienLRY <83220559+aurelienLRY@users.noreply.github.com>
2025-01-02 16:24:23 +0100

    Enhance PWA support and update configuration
    - Updated .gitignore to exclude auto-generated PWA files.
    - Modified next.config.ts to enable SWC minification and remove console logs in production.
    - Added metadata for the application in layout.tsx, including title, description, and icons for PWA.
    - Updated RootLayout to use French language for improved localization.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import withPWA from "next-pwa";
 
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import { Providers } from "./providers";
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Swissborg UI",
+  description: "Swissborg UI",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icons/android-chrome-192x192.png",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="fr" suppressHydrationWarning>
       <body>
         <Providers>{children}</Providers>
       </body>
