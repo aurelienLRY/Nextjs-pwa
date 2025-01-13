0e7302b39a25a146a86306e8bb3e0b3b00e44cc4
aurelienLRY <83220559+aurelienLRY@users.noreply.github.com>
2025-01-13 09:40:09 +0100

    Remove manifest files and enhance layout metadata for Swissborg UI
    - Deleted the manifest.json and manifeste.ts files as part of the PWA configuration cleanup.
    - Updated layout.tsx to include additional metadata such as keywords, viewport settings, and author information for improved SEO and accessibility.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,20 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Swissborg UI",
   description: "Swissborg UI",
+  keywords: ["Swissborg", "UI", "Next.js", "React", "Tailwind", "Shadcn"],
   generator: "Next.js",
   manifest: "/manifest.json",
   icons: {
     icon: "/icons/android-chrome-192x192.png",
   },
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  authors: [
+    {
+      name: "Leroy Aurélien",
+      url: "https://www.linkedin.com/in/aur%C3%A9lien-leroy-8304a9284/",
+    },
+  ],
 };
 
 export default function RootLayout({
